8688c74846854152fd486899f88a944b72d2a63b
Rodrigo Alejandro Mora Azocar <brazuk25@gmail.com>
2023-10-09 21:23:37 -0300

    Creación del Proyecto APIREST CON ANGULAR

---
+++ src/main/java/br/com/roroprojects/apiconangular/ApiconangularApplication.java
@@ -0,0 +1,13 @@
+package br.com.roroprojects.apiconangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiconangularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiconangularApplication.class, args);
+	}
+
+}
