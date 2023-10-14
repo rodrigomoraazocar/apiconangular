9faa54df6e8d516ec884656b27def4985ca011b7
Rodrigo Alejandro Mora Azocar <brazuk25@gmail.com>
2023-10-13 22:53:12 -0300

    Cors y GetMapping - Alteraciones

--- src/main/java/br/com/roroprojects/apiconangular/controller/Controle.java
+++ src/main/java/br/com/roroprojects/apiconangular/controller/Controle.java
@@ -1,6 +1,7 @@
 package br.com.roroprojects.apiconangular.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import br.com.roroprojects.apiconangular.model.Cliente;
 import br.com.roroprojects.apiconangular.repository.Repositorio;
 
 @RestController
+@CrossOrigin(origins = "*") //Puerta del front end 
 public class Controle {
 
 	@Autowired
@@ -24,7 +26,7 @@ public class Controle {
 		return acao.save(c);
 	}
 	
-	@GetMapping("/")
+	@GetMapping("/listar")
 	public Iterable<Cliente> selecionar () {
 		
 		return acao.findAll();
