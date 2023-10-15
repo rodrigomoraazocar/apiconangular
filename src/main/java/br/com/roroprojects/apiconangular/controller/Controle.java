301c8d77a8a3bcf0601cb66e3c780b29fe30e51e
Rodrigo Alejandro Mora Azocar <brazuk25@gmail.com>
2023-10-15 17:05:58 -0300

    Conf localmente

--- src/main/java/br/com/roroprojects/apiconangular/controller/Controle.java
+++ src/main/java/br/com/roroprojects/apiconangular/controller/Controle.java
@@ -1,7 +1,7 @@
 package br.com.roroprojects.apiconangular.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +14,7 @@ import br.com.roroprojects.apiconangular.model.Cliente;
 import br.com.roroprojects.apiconangular.repository.Repositorio;
 
 @RestController
-//@CrossOrigin(origins = "https://apiconangular-frontend-3v8nk7zvw-rodrigomoraazocars-projects.vercel.app") //Puerta del front end 
+@CrossOrigin (origins = "http://localhost:4200") //Puerta del front end 
 public class Controle {
 
 	@Autowired
